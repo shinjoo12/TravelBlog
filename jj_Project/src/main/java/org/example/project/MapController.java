5b99b991de4a3d6355180b6eb85c43415be00c1b
{joo} <{zhuyaan93@gmail.com}>
2024-06-24 18:30:30 +0900

    6/24

--- jj_Project/src/main/java/org/example/project/MapController.java
+++ jj_Project/src/main/java/org/example/project/MapController.java
@@ -1,4 +1,10 @@
 
+package org.example.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class MapController {
 
